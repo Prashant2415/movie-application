42562365cd341e64f4577377b7e3b579ef41eccd
“Prashant2415” <prashantmendhejbp@gmail.com>
2025-04-05 21:12:11 +0530

    Responsive design

--- movie-app/src/component/layout/Footer.jsx
+++ movie-app/src/component/layout/Footer.jsx
@@ -3,26 +3,6 @@ import "./layout.css"
 import { Link } from 'react-router'
 const Footer = () => {
   return (
-    // <footer className="footer">
-    //   <div className="footer-content">
-    //     <p>© 2025 MovieMania. All rights reserved.</p>
-
-    //     <div className="footer-links">
-    //       <a href="/">Home</a>
-    //       <a href="/movies">Movies</a>
-    //       <a href="/about">About</a>
-    //       <a href="/contact">Contact</a>
-    //     </div>
-
-    //     <div className="footer-socials">
-    //       <a href="#"><img src="/icons/facebook.svg" alt="Facebook" /></a>
-    //       <a href="#"><img src="/icons/instagram.svg" alt="Instagram" /></a>
-    //       <a href="#"><img src="/icons/twitter.svg" alt="Twitter" /></a>
-    //     </div>
-
-    //     <p className="footer-credit">Made with ❤️ by Prashant Mendhe</p>
-    //   </div>
-    // </footer>
     <footer className='footer-container'>
       <div className="footer-content-container">
         <p className='copyright'>© 2025 PMMovies. All rights reserved.</p>
@@ -32,7 +12,6 @@ const Footer = () => {
           <Link className='footer-nav-link' to="/movies">Movies</Link>
           <Link className='footer-nav-link' to="/about">About</Link>
           </div>
-          {/* <p className="footer-credit">Made with ❤️ by Prashant Mendhe</p> */}
         </div>
         <p className="footer-credit">Made with ❤️ by Prashant Mendhe</p>
       </div>
--- movie-app/src/component/pages/Home.jsx
+++ movie-app/src/component/pages/Home.jsx
@@ -1,18 +1,15 @@
 import React from 'react'
 import "./pages.css"
 import Carousel from '../selfcomponent/carousel/Carousel'
-import { useDispatch, useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
 import ImageCard from '../selfcomponent/card/ImageCard'
 import { BorderedSubHeading } from '../selfcomponent/Text/CommonText'
-import { addToWatchlist } from '../../redux/movieSlice'
 import { useNavigate } from 'react-router'
 const Home = () => {
   const movieData = useSelector((state) => state.movieSlice.movieData)
   const filterRatingData = movieData.filter((item) => item.rating > 8.5);
   const navigate = useNavigate();
-  const dispatch = useDispatch();
   const handleAddToWatchlist=(movieItem)=>{
-    // dispatch(addToWatchlist(data))
     navigate("/movieItem",{state: movieItem})
   }  
   return (
